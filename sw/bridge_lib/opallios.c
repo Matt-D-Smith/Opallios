a0b899098e18143ee667048cf925290964125553
Matt-D-Smith <mail@mattsmith.ee>
2022-08-08 22:30:21 +0000

    get gifs working, compile raylib for GLES2

--- sw/bridge_lib/opallios.c
+++ sw/bridge_lib/opallios.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <raylib.h>
 #include <string.h>
+#include <unistd.h>
 #include "bw_bridge.h"
 
 int main(int argc, char *argv[])
@@ -13,9 +14,10 @@ int main(int argc, char *argv[])
 	int c;
 
 	char filename[256];
-	uint16_t matrixData[8192];
+	int numFrames;
+	Image img;
 
-	static struct option long_opts[]=
+	static struct option long_opts[]= //parse arguments to read file name with -f
 	{
 		{ "filename", required_argument, 0, 'f' },
 		{ 0, 0, 0, 0 },
@@ -32,20 +34,45 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	Image img = LoadImage(filename);
-
-	Color *imgColors = LoadImageColors(img);
-
-	for (int i = 0; i < img.width * img.height; i++)
-	{
-		(matrixData)[i*2] = (imgColors[i].g >> 2) << 8 | (imgColors[i].r >> 2);
-		(matrixData)[i*2+1] = (imgColors[i].b >> 2);
+	if (IsFileExtension(filename, ".png")) { // see if we are loading an image or an animation
+		img = LoadImage(filename);
+		numFrames = 1;
+	}
+	else if (IsFileExtension(filename, ".gif")) {
+		img = LoadImageAnim(filename, &numFrames);
+	}
+	else {
+		printf("ERROR: File type not supported");
+		return 1;
 	}
 
-	if (bridge_init(&br, BW_BRIDGE_MEM_ADR, BW_BRIDGE_MEM_SIZE) < 0)
-		return -1;
+	if (bridge_init(&br, BW_BRIDGE_MEM_ADR, BW_BRIDGE_MEM_SIZE) < 0) { //after loading in file, initialize the GPMC interface
+		printf("ERROR: GPMC Bridge Init failed");
+		return 2;
+	}
 
-	set_fpga_mem(&br, 0x4000, matrixData, 8192);
+	int currentFrame = 0;
+	int numPixels = img.width * img.height;
+
+	printf("Number of Frames: %d\n", numFrames);
+
+	uint16_t matrixData[8192];
+	do {
+		
+		for (int i = 0; i < numPixels; i++)
+		{
+			(matrixData)[i*2] = (((uint8_t *)img.data)[i*4+1+currentFrame*numPixels*4] >> 2) << 8 | (((uint8_t *)img.data)[i*4+currentFrame*numPixels*4] >> 2);
+			(matrixData)[i*2+1] = (((uint8_t *)img.data)[i*4+2+currentFrame*numPixels*4] >> 2);
+		}
+		currentFrame++;
+		if (currentFrame >= numFrames) currentFrame = 0;
+
+		printf("currentFrame %d\n", currentFrame);
+
+		set_fpga_mem(&br, 0x4000, matrixData, 8192);
+		
+		usleep(16000);
+	} while (numFrames > 1);
 
 	bridge_close(&br);
 	UnloadImage(img);         // Unload CPU (RAM) image data (pixels)
