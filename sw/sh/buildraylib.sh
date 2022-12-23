654fad9cb6bca5687d71a57107905883ad4009fe
Matt-D-Smith <mail@mattsmith.ee>
2022-12-23 16:14:43 +0000

    remove GPU files, BBB GPU is horrible

--- sw/sh/buildraylib.sh
+++ sw/sh/buildraylib.sh
@@ -1,5 +1,5 @@
 cd /home/debian/raylib/src
-make PLATFORM=PLATFORM_DESKTOP GRAPHICS=GRAPHICS_API_OPENGL_ES2
+make PLATFORM=PLATFORM_DRM GRAPHICS=GRAPHICS_API_OPENGL_ES2
 make install
 cp libraylib.a /home/debian/Opallios/sw/opallios
 cd /home/debian/Opallios/sw/opallios
